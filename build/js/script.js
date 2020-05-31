490fa8fc9012306f3041c5ecc317a1e9f259ac4f
ArthurKuchuk <arthur.bearnyson@gmail.com>
2020-05-31 22:05:05 +0600

    Сделал модалки и анимацию

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 $('.slider-projects').slick({
   lazyLoad: 'ondemand',
 		slidesToShow: 3,
@@ -21,7 +22,30 @@ $('.slider-projects').slick({
 		},
 		]
 	});
-  
-  function txt_2_clpbrd(){
-    window.clipboardData.setData("Text","muxamatnurovna@mail.ru");
-  }
+  /////////////////////////////
+let modalOpen = document.querySelector(".modal__open-1");
+let modalOpen2 = document.querySelector(".modal__open-2");
+let modalOpen3 = document.querySelector(".modal__open-3");
+
+let modalForm = document.querySelector(".modal");
+let modalClose = document.querySelector(".modal__close");
+let modalFormOverlay = document.querySelector(".modal__overlay");
+
+
+let showModal = function(elementName) {
+  elementName.classList.add("show");
+}
+
+let hideModal = function(elementName) { 
+  elementName.classList.remove("show");
+}
+
+
+modalOpen.addEventListener("click", () => showModal(modalForm));
+modalOpen2.addEventListener("click", () => showModal(modalForm));
+modalOpen3.addEventListener("click", () => showModal(modalForm));
+
+modalClose.addEventListener("click", () => hideModal(modalForm));
+modalFormOverlay.addEventListener("click", () => hideModal(modalForm));
+////////////////////////////
+new WOW().init();
